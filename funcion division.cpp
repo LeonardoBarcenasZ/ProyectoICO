8ee5b49e0a434edf35c28f4af2ada016b20eec42
BrianCuevas02 <91903843+BrianCuevas02@users.noreply.github.com>
2021-12-20 18:39:35 -0600

    Add files via upload

---
+++ funcion division.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   
+	float numero1, numero2, division;
+
+	cout << "Ingrese el numero 1:";
+	cin >> numero1;
+
+	cout << "Ingrese el numero 2:";
+	cin >> numero2;
+
+	division = numero1 / numero2;
+
+	cout << "La division de" << numero1 << "/" << numero2 << "es:" << division<<endl;
+
+	return 0;
+
+
+
+
+
+
+
+
+}
+
