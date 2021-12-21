d1e148f0f53f064a764a33e04b20219ae64ce8e8
Leonardo <91977761+LeonardoBarcenasZ@users.noreply.github.com>
2021-12-20 19:05:53 -0600

    Update ArchivoProyecto.c

--- ArchivoProyecto.c
+++ ArchivoProyecto.c
@@ -1,16 +1,1 @@
-//Aqui estaran las funciones (suema, resta, multiplicacion, division)
-#include <iostream>
 
-int main ()
-{
-  
-    int n1,n2,resta;
-    printf("Introducir una cantidad");
-    scanf("%d", &n1);
-
-    printf("Introducir una segunda cantidad");
-    scanf("%d", &n2);
-
-    resta=n1-n2;
-    printf("La respuesta es:%d", resta);
-}
